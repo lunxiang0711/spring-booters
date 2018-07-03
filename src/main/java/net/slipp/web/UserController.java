4017e9aebfc9f85f71bba70c31872e31fb5c7d75
Kim Yoon Sung <yoonsung0711@gmail.com>
2018-07-03 15:33:36 +0900

    (U)pdate src/main/java/net/slipp/web/UserController.java : saving Http Parameters into arraylist and binding it with Model

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -1,13 +1,25 @@
 package net.slipp.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+	private List<User> users = new ArrayList<User>();
 	@PostMapping(value = "/create")
 	public String create(User user) {
 		System.out.println(user);
+		users.add(user);
 		return "result";
+	}
+	@GetMapping("/list")
+	public String list(Model model) {
+		model.addAttribute("users", users); 
+		return "list";
 	}
 }
