381144c1671a0caa134136ca7d7cffda5acd9fc7
Kim Yoon Sung <yoonsung0711@gmail.com>
2018-07-03 14:56:39 +0900

    (U)pdate src/main/java/net/slipp/web/User.java : Binding Model with Http Parameters

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class UserController {
 	@PostMapping(value = "/create")
-	public String create(String userId, String password, String name, String email) {
-		System.out.println("userId " + userId + "\npassword " + password + "\nname " + name + "\nemail " + email);
+	public String create(User user) {
+		System.out.println(user);
 		return "result";
 	}
 }
