96942c19c44c1664bd0c5d107c48cce6fa7ea90e
Kim Yoon Sung <yoonsung0711@gmail.com>
2018-07-03 14:12:00 +0900

    (U)pdate src/main/resources/templates/welcome.html : Test Mustache Interpolation

--- src/main/java/net/slipp/web/WelcomeController.java
+++ src/main/java/net/slipp/web/WelcomeController.java
@@ -1,12 +1,16 @@
 package net.slipp.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomeController {
 	@GetMapping(value = "/helloworld")
-	public String welcome() {
+	public String welcome(String name, int age, Model model) {
+		model.addAttribute("name", name);
+		model.addAttribute("age", age);
+		System.out.println(name);
 		return "welcome";
 	}
 }
